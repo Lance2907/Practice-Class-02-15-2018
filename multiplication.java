f760998286b516f16705b2a605e790e58d26140f
Latta <lslatta.2015@mymail.jcsu.edu>
2018-02-15 13:20:48 -0500

    first commit

---
+++ multiplication.java
@@ -0,0 +1,15 @@
+
+public class multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+
+int num1=2;
+int num2=3;
+int num3=10;
+int num4=(num1*num2*num3);
+System.out.println("2*3*0 equals" + num4);
+}
+}
